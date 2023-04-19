cc4edf7756079f7e28dcbcf93ad62e2e00061da6
JIE <710578873@qq.com>
2023-04-19 22:49:51 +0800

    279. 完全平方数

---
+++ src/com/test/jie/leetCode/dp/PerfectSquares.java
@@ -0,0 +1,40 @@
+package com.test.jie.leetCode.dp;
+
+import java.util.Arrays;
+
+/**
+ * 279. 完全平方数
+ * 中等
+ * 给你一个整数 n ，返回 和为 n 的完全平方数的最少数量 。
+ * 完全平方数 是一个整数，其值等于另一个整数的平方；换句话说，其值等于一个整数自乘的积。
+ * * 例如，1、4、9 和 16 都是完全平方数，而 3 和 11 不是。
+ * <p>
+ * 示例 1：
+ * 输入：n = 12
+ * 输出：3
+ * 解释：12 = 4 + 4 + 4
+ * 示例 2：
+ * 输入：n = 13
+ * 输出：2
+ * 解释：13 = 4 + 9
+ * <p>
+ * 提示：
+ * 1 <= n <= 104
+ */
+public class PerfectSquares {
+    public static int numSquares(int n) {
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, n + 1);
+        dp[0] = 0;
+        for (int i = 1; i * i <= n; i++) {
+            for (int j = i * i; j <= n; j++) {
+                dp[j] = Math.min(dp[j], dp[j - i * i] + 1);
+            }
+        }
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        numSquares(12);
+    }
+}
