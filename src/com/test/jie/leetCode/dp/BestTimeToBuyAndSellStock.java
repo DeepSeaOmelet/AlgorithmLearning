4027ff6b60df8cb013bdde3b0486d8570f1aba4c
JIE <710578873@qq.com>
2023-04-27 18:23:54 +0800

    买卖股票的最佳时机III

---
+++ src/com/test/jie/leetCode/dp/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,179 @@
+package com.test.jie.leetCode.dp;
+
+import java.util.Arrays;
+
+//买卖股票的最佳时机
+public class BestTimeToBuyAndSellStock {
+    /**
+     * 121. 买卖股票的最佳时机   https://leetcode.cn/problems/best-time-to-buy-and-sell-stock/
+     * 简单
+     * 给定一个数组 prices ，它的第 i 个元素 prices[i] 表示一支给定股票第 i 天的价格。
+     * 你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。设计一个算法来计算你所能获取的最大利润。
+     * 返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0 。
+     * <p>
+     * 示例 1：
+     * 输入：[7,1,5,3,6,4]
+     * 输出：5
+     * 解释：在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。
+     * 注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格；同时，你不能在买入前卖出股票。
+     * 示例 2：
+     * 输入：prices = [7,6,4,3,1]
+     * 输出：0
+     * 解释：在这种情况下, 没有交易完成, 所以最大利润为 0。
+     * <p>
+     * 提示：
+     * 1 <= prices.length <= 105
+     * 0 <= prices[i] <= 104
+     *
+     * @param prices
+     * @return
+     */
+    public int maxProfit2(int[] prices) {
+        //dp[i][1]表示第i天不持有股票所得最多现金,dp[i][0] 表示第i天持有股票所得最多现金
+        int n = prices.length;
+        //1
+        int[][] dp = new int[n][2];
+        //2
+        dp[0][0] = -prices[0];
+        //3
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], -prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][0] + prices[i], dp[i - 1][1]);
+        }
+        return dp[n - 1][1];
+    }
+
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int ans = 0;
+        int min = prices[0];
+        for (int i = 0; i < n; i++) {
+            if (min > prices[i]) {
+                min = prices[i];
+            } else {
+                ans = Math.max(ans, prices[i] - min);
+            }
+        }
+        return ans;
+    }
+}
+
+/**
+ * 122. 买卖股票的最佳时机 II    https://leetcode.cn/problems/best-time-to-buy-and-sell-stock-ii/
+ * 中等
+ * 给你一个整数数组 prices ，其中 prices[i] 表示某支股票第 i 天的价格。
+ * 在每一天，你可以决定是否购买和/或出售股票。你在任何时候 最多 只能持有 一股 股票。你也可以先购买，然后在 同一天 出售。
+ * 返回 你能获得的 最大 利润 。
+ * <p>
+ * 示例 1：
+ * 输入：prices = [7,1,5,3,6,4]
+ * 输出：7
+ * 解释：在第 2 天（股票价格 = 1）的时候买入，在第 3 天（股票价格 = 5）的时候卖出, 这笔交易所能获得利润 = 5 - 1 = 4 。
+ * 随后，在第 4 天（股票价格 = 3）的时候买入，在第 5 天（股票价格 = 6）的时候卖出, 这笔交易所能获得利润 = 6 - 3 = 3 。
+ * 总利润为 4 + 3 = 7 。
+ * 示例 2：
+ * 输入：prices = [1,2,3,4,5]
+ * 输出：4
+ * 解释：在第 1 天（股票价格 = 1）的时候买入，在第 5 天 （股票价格 = 5）的时候卖出, 这笔交易所能获得利润 = 5 - 1 = 4 。
+ * 总利润为 4 。
+ * 示例 3：
+ * 输入：prices = [7,6,4,3,1]
+ * 输出：0
+ * 解释：在这种情况下, 交易无法获得正利润，所以不参与交易可以获得最大利润，最大利润为 0 。
+ * <p>
+ * 提示：
+ * 1 <= prices.length <= 3 * 104
+ * 0 <= prices[i] <= 104
+ */
+class Solution2 {
+    public int maxProfit2(int[] prices) {
+        int n = prices.length;
+        int[][] dp = new int[n][2];
+        //0为未持有，1为持有
+        dp[0][1] = -prices[0];
+        for (int i = 1; i < n; i++) {
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+        }
+        return dp[n - 1][0];
+    }
+
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int ans = 0;
+        int min = prices[0];
+        for (int i = 1; i < n; i++) {
+            if (prices[i] > min) {
+                ans += prices[i] - min;
+            }
+            min = prices[i];
+        }
+        return ans;
+    }
+}
+
+/**
+ * 123. 买卖股票的最佳时机 III   https://leetcode.cn/problems/best-time-to-buy-and-sell-stock-iii/
+ * 困难
+ * 给定一个数组，它的第 i 个元素是一支给定的股票在第 i 天的价格。
+ * 设计一个算法来计算你所能获取的最大利润。你最多可以完成 两笔 交易。
+ * 注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+ * <p>
+ * 示例 1:
+ * 输入：prices = [3,3,5,0,0,3,1,4]
+ * 输出：6
+ * 解释：在第 4 天（股票价格 = 0）的时候买入，在第 6 天（股票价格 = 3）的时候卖出，这笔交易所能获得利润 = 3-0 = 3 。
+ * 随后，在第 7 天（股票价格 = 1）的时候买入，在第 8 天 （股票价格 = 4）的时候卖出，这笔交易所能获得利润 = 4-1 = 3 。
+ * 示例 2：
+ * 输入：prices = [1,2,3,4,5]
+ * 输出：4
+ * 解释：在第 1 天（股票价格 = 1）的时候买入，在第 5 天 （股票价格 = 5）的时候卖出, 这笔交易所能获得利润 = 5-1 = 4 。
+ * 注意你不能在第 1 天和第 2 天接连购买股票，之后再将它们卖出。
+ * 因为这样属于同时参与了多笔交易，你必须在再次购买前出售掉之前的股票。
+ * 示例 3：
+ * 输入：prices = [7,6,4,3,1]
+ * 输出：0
+ * 解释：在这个情况下, 没有交易完成, 所以最大利润为 0。
+ * 示例 4：
+ * 输入：prices = [1]
+ * 输出：0
+ * <p>
+ * 提示：
+ * 1 <= prices.length <= 105
+ * 0 <= prices[i] <= 105
+ */
+class Solution3 {
+    public int maxProfit(int[] prices) {
+        //空间优化
+        int[] dp = new int[4];
+        //0第一次持有，1第一次不持有，2第二次持有，3第二次不持有
+        dp[0] = -prices[0];
+        dp[1] = 0;
+        dp[2] = -prices[0];
+        dp[3] = 0;
+        for (int i = 1; i < prices.length; i++) {
+            dp[0] = Math.max(dp[0], -prices[i]);
+            dp[1] = Math.max(dp[1], dp[0] + prices[i]);
+            //这里保存这一次的第一次卖出
+            dp[2] = Math.max(dp[2], dp[1] - prices[i]);
+            dp[3] = Math.max(dp[3], dp[2] + prices[i]);
+        }
+        return dp[3];
+    }
+
+    public int maxProfit2(int[] prices) {
+        int n = prices.length;
+        int[][] dp = new int[n][4];
+        //0第一次持有，1第一次不持有，2第二次持有，3第二次不持有
+        dp[0][0] = -prices[0];
+        dp[0][2] = -prices[0];
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], -prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] + prices[i]);
+            dp[i][2] = Math.max(dp[i - 1][2], dp[i - 1][1] - prices[i]);
+            dp[i][3] = Math.max(dp[i - 1][3], dp[i - 1][2] + prices[i]);
+        }
+        return dp[n - 1][3];
+    }
+
+}
