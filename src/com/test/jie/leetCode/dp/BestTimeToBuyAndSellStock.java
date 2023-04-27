482c31b48dbb692d0a04f5be8801e5907b7bcbe5
JIE <710578873@qq.com>
2023-04-27 21:13:12 +0800

    309. 最佳买卖股票时机含冷冻期

--- src/com/test/jie/leetCode/dp/BestTimeToBuyAndSellStock.java
+++ src/com/test/jie/leetCode/dp/BestTimeToBuyAndSellStock.java
@@ -210,11 +210,48 @@ class Solution4 {
         }
         for (int i = 1; i < n; i++) {
             for (int j = 1; j < dp.length; j += 2) {
+                //j为持有，j+1为未持有
                 dp[j] = Math.max(dp[j], dp[j - 1] - prices[i]);
                 dp[j + 1] = Math.max(dp[j + 1], dp[j] + prices[i]);
             }
             System.out.println(Arrays.toString(dp));
         }
         return dp[dp.length - 1];
+    }
+}
+
+/**
+ * 309. 最佳买卖股票时机含冷冻期
+ * 中等
+ * 给定一个整数数组prices，其中第  prices[i] 表示第 i 天的股票价格 。​
+ * 设计一个算法计算出最大利润。在满足以下约束条件下，你可以尽可能地完成更多的交易（多次买卖一支股票）:
+ * 卖出股票后，你无法在第二天买入股票 (即冷冻期为 1 天)。
+ * 注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+ * <p>
+ * 示例 1:
+ * 输入: prices = [1,2,3,0,2]
+ * 输出: 3
+ * 解释: 对应的交易状态为: [买入, 卖出, 冷冻期, 买入, 卖出]
+ * 示例 2:
+ * 输入: prices = [1]
+ * 输出: 0
+ * <p>
+ * 提示：
+ * 1 <= prices.length <= 5000
+ * 0 <= prices[i] <= 1000
+ */
+class Solution5 {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        //0为持有，1为未持有股票，不包括今天卖出，2今天卖出股票，3冷冻期
+        int[][] dp = new int[n][4];
+        dp[0][0] = -prices[0];
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], Math.max(dp[i - 1][1], dp[i - 1][3]) - prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][3], dp[i - 1][1]);
+            dp[i][2] = dp[i - 1][0] + prices[i];
+            dp[i][3] = dp[i - 1][2];
+        }
+        return Math.max(Math.max(dp[n - 1][2], dp[n - 1][1]), dp[n - 1][3]);
     }
 }
