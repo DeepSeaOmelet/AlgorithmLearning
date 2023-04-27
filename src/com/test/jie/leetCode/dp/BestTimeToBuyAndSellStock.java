31b5e4ad28afd92427db5070d05dea81f7642e25
JIE <710578873@qq.com>
2023-04-27 20:37:58 +0800

    买卖股票的最佳时机IV

--- src/com/test/jie/leetCode/dp/BestTimeToBuyAndSellStock.java
+++ src/com/test/jie/leetCode/dp/BestTimeToBuyAndSellStock.java
@@ -175,5 +175,46 @@ class Solution3 {
         }
         return dp[n - 1][3];
     }
+}
 
+/**
+ * 188. 买卖股票的最佳时机 IV    https://leetcode.cn/problems/best-time-to-buy-and-sell-stock-iv/
+ * 困难
+ * 给定一个整数数组 prices ，它的第 i 个元素 prices[i] 是一支给定的股票在第 i 天的价格，和一个整型 k 。
+ * 设计一个算法来计算你所能获取的最大利润。你最多可以完成 k 笔交易。也就是说，你最多可以买 k 次，卖 k 次。
+ * 注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+ * <p>
+ * 示例 1：
+ * 输入：k = 2, prices = [2,4,1]
+ * 输出：2
+ * 解释：在第 1 天 (股票价格 = 2) 的时候买入，在第 2 天 (股票价格 = 4) 的时候卖出，这笔交易所能获得利润 = 4-2 = 2 。
+ * 示例 2：
+ * 输入：k = 2, prices = [3,2,6,5,0,3]
+ * 输出：7
+ * 解释：在第 2 天 (股票价格 = 2) 的时候买入，在第 3 天 (股票价格 = 6) 的时候卖出, 这笔交易所能获得利润 = 6-2 = 4 。
+ * 随后，在第 5 天 (股票价格 = 0) 的时候买入，在第 6 天 (股票价格 = 3) 的时候卖出, 这笔交易所能获得利润 = 3-0 = 3 。
+ * <p>
+ * 提示：
+ * 0 <= k <= 100
+ * 0 <= prices.length <= 1000
+ * 0 <= prices[i] <= 1000
+ */
+class Solution4 {
+    public int maxProfit(int k, int[] prices) {
+        int[] dp = new int[2 * k + 1];
+        int n = prices.length;
+        for (int i = 0; i < dp.length; i++) {
+            if (i % 2 == 1) {
+                dp[i] = -prices[0];
+            }
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < dp.length; j += 2) {
+                dp[j] = Math.max(dp[j], dp[j - 1] - prices[i]);
+                dp[j + 1] = Math.max(dp[j + 1], dp[j] + prices[i]);
+            }
+            System.out.println(Arrays.toString(dp));
+        }
+        return dp[dp.length - 1];
+    }
 }
