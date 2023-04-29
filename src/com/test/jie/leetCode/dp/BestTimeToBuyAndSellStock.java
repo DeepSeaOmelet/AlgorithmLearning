238f48ffde65e9459462d3dd0497e0e21c922390
JIE <710578873@qq.com>
2023-04-29 11:37:00 +0800

    多种写法

--- src/com/test/jie/leetCode/dp/BestTimeToBuyAndSellStock.java
+++ src/com/test/jie/leetCode/dp/BestTimeToBuyAndSellStock.java
@@ -241,6 +241,36 @@ class Solution4 {
  * 0 <= prices[i] <= 1000
  */
 class Solution5 {
+    public int maxProfit3(int[] prices) {
+        int n = prices.length;
+        //0为持有，1为未持有
+        int[][] dp = new int[n + 1][2];
+        dp[1][0] = -prices[0];
+        for (int i = 2; i <= n; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 2][1] - prices[i - 1]);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] + prices[i - 1]);
+        }
+        return dp[n][1];
+    }
+
+    public int maxProfit2(int[] prices) {
+        if (prices == null || prices.length < 2) {
+            return 0;
+        }
+        int[][] dp = new int[prices.length][2];
+
+        //计算第二天的情况
+        dp[0][0] = -prices[0];
+        dp[0][1] = 0;
+        dp[1][0] = Math.max(dp[0][0], -prices[1]);
+        dp[1][1] = Math.max(dp[0][1], dp[0][0] + prices[1]);
+        for (int i = 2; i < prices.length; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 2][1] - prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] + prices[i]);
+        }
+        return dp[prices.length - 1][1];
+    }
+
     public int maxProfit(int[] prices) {
         int n = prices.length;
         //0为持有，1为未持有股票，不包括今天卖出，2今天卖出股票，3冷冻期
@@ -250,7 +280,7 @@ class Solution5 {
             dp[i][0] = Math.max(dp[i - 1][0], Math.max(dp[i - 1][1], dp[i - 1][3]) - prices[i]);
             dp[i][1] = Math.max(dp[i - 1][3], dp[i - 1][1]);
             dp[i][2] = dp[i - 1][0] + prices[i];
-            dp[i][3] = dp[i - 1][2];
+            dp[i][3] = dp[i - 1][2];//表明今天是冷冻期
         }
         return Math.max(Math.max(dp[n - 1][2], dp[n - 1][1]), dp[n - 1][3]);
     }
