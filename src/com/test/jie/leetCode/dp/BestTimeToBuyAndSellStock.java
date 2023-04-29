87f393d9f07c864cd6f1f6f9d83176f2e188d6b5
JIE <710578873@qq.com>
2023-04-29 17:14:17 +0800

    714. 买卖股票的最佳时机含手续费

--- src/com/test/jie/leetCode/dp/BestTimeToBuyAndSellStock.java
+++ src/com/test/jie/leetCode/dp/BestTimeToBuyAndSellStock.java
@@ -245,6 +245,7 @@ class Solution5 {
         int n = prices.length;
         //0为持有，1为未持有
         int[][] dp = new int[n + 1][2];
+        //优化初始值
         dp[1][0] = -prices[0];
         for (int i = 2; i <= n; i++) {
             dp[i][0] = Math.max(dp[i - 1][0], dp[i - 2][1] - prices[i - 1]);
@@ -277,11 +278,80 @@ class Solution5 {
         int[][] dp = new int[n][4];
         dp[0][0] = -prices[0];
         for (int i = 1; i < n; i++) {
+            //Math.max(dp[i - 1][1], dp[i - 1][3]) - prices[i]，上一天是冷冻期，或者是未持有股票
             dp[i][0] = Math.max(dp[i - 1][0], Math.max(dp[i - 1][1], dp[i - 1][3]) - prices[i]);
             dp[i][1] = Math.max(dp[i - 1][3], dp[i - 1][1]);
             dp[i][2] = dp[i - 1][0] + prices[i];
-            dp[i][3] = dp[i - 1][2];//表明今天是冷冻期
+            dp[i][3] = dp[i - 1][2];//上一天是卖出，表明今天是冷冻期
         }
         return Math.max(Math.max(dp[n - 1][2], dp[n - 1][1]), dp[n - 1][3]);
+    }
+}
+
+/**
+ * 714. 买卖股票的最佳时机含手续费
+ * 中等
+ * 给定一个整数数组 prices，其中 prices[i]表示第 i 天的股票价格 ；整数 fee 代表了交易股票的手续费用。
+ * 你可以无限次地完成交易，但是你每笔交易都需要付手续费。如果你已经购买了一个股票，在卖出它之前你就不能再继续购买股票了。
+ * 返回获得利润的最大值。
+ * <p>
+ * 注意：这里的一笔交易指买入持有并卖出股票的整个过程，每笔交易你只需要为支付一次手续费。
+ * <p>
+ * 示例 1：
+ * 输入：prices = [1, 3, 2, 8, 4, 9], fee = 2
+ * 输出：8
+ * 解释：能够达到的最大利润:
+ * 在此处买入 prices[0] = 1
+ * 在此处卖出 prices[3] = 8
+ * 在此处买入 prices[4] = 4
+ * 在此处卖出 prices[5] = 9
+ * 总利润: ((8 - 1) - 2) + ((9 - 4) - 2) = 8
+ * 示例 2：
+ * 输入：prices = [1,3,7,5,10,3], fee = 3
+ * 输出：6
+ * <p>
+ * 提示：
+ * 1 <= prices.length <= 5 * 104
+ * 1 <= prices[i] < 5 * 104
+ * 0 <= fee < 5 * 104
+ */
+class Solution6 {
+    public int maxProfit3(int[] prices, int fee) {
+        int n = prices.length;
+        int buy = prices[0] + fee;
+        int ans = 0;
+        for (int i = 1; i < n; i++) {
+            if (prices[i] > buy) {
+                ans += prices[i] - buy;
+                buy = prices[i];
+            } else if (prices[i] + fee < buy) {
+                buy = prices[i] + fee;
+            }
+        }
+        return ans;
+    }
+
+    public int maxProfit2(int[] prices, int fee) {
+        int n = prices.length;
+        int[] dp = new int[2];
+        dp[0] = -prices[0];
+        for (int i = 1; i < n; i++) {
+            dp[0] = Math.max(dp[0], dp[1] - prices[i]);
+            dp[1] = Math.max(dp[1], dp[0] + prices[i] - fee);
+        }
+        return dp[1];
+    }
+
+    public int maxProfit(int[] prices, int fee) {
+        int n = prices.length;
+        int[][] dp = new int[n][2];
+        //0为持有，1为未持有
+        dp[0][0] = -prices[0];
+        for (int i = 1; i < n; i++) {
+            //可以无限次交易
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] - prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] + prices[i] - fee);
+        }
+        return dp[n - 1][1];
     }
 }
