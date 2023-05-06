36e6bde00e9ae241335e54c7ba6de9afdbab6a49
JIE <710578873@qq.com>
2023-05-06 11:58:06 +0800

    53. 最大子数组和

--- src/com/test/jie/leetCode/dp/Subsequences.java
+++ src/com/test/jie/leetCode/dp/Subsequences.java
@@ -262,7 +262,7 @@ class SubsequencesSolution5 {
             for (int j = 1; j <= nums2.length; j++) {
                 if (nums1[i - 1] == nums2[j - 1]) {
                     dp[i][j] = dp[i - 1][j - 1] + 1;
-                    ans = Math.max(dp[i][j],ans);
+                    ans = Math.max(dp[i][j], ans);
                 } else {
                     dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 }
@@ -270,6 +270,42 @@ class SubsequencesSolution5 {
         }
         for (int[] d : dp) {
             System.out.println(Arrays.toString(d));
+        }
+        return ans;
+    }
+}
+
+/**
+ * 53. 最大子数组和
+ * 中等
+ * 给你一个整数数组 nums ，请你找出一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+ * 子数组 是数组中的一个连续部分。
+ * <p>
+ * 示例 1：
+ * 输入：nums = [-2,1,-3,4,-1,2,1,-5,4]
+ * 输出：6
+ * 解释：连续子数组 [4,-1,2,1] 的和最大，为 6 。
+ * 示例 2：
+ * 输入：nums = [1]
+ * 输出：1
+ * 示例 3：
+ * 输入：nums = [5,4,-1,7,8]
+ * 输出：23
+ * <p>
+ * 提示：
+ * 1 <= nums.length <= 105
+ * -104 <= nums[i] <= 104
+ * <p>
+ * 进阶：如果你已经实现复杂度为 O(n) 的解法，尝试使用更为精妙的 分治法 求解。
+ */
+class SubsequencesSolution6 {
+    public int maxSubArray(int[] nums) {
+        int[] dp = new int[nums.length + 1];
+        dp[0] = nums[0];
+        int ans = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+            ans = Math.max(ans, dp[i]);
         }
         return ans;
     }
