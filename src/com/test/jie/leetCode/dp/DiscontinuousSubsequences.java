aaea14b252bc7db00a1cd80f48372fc61bc35afb
JIE <710578873@qq.com>
2023-05-06 11:30:00 +0800

    1035. 不相交的线

--- src/com/test/jie/leetCode/dp/DiscontinuousSubsequences.java
+++ src/com/test/jie/leetCode/dp/DiscontinuousSubsequences.java
@@ -2,7 +2,7 @@ package com.test.jie.leetCode.dp;
 
 import java.util.Arrays;
 
-public class DiscontinuousSubsequences {
+public class Subsequences {
     /**
      * 300. 最长递增子序列
      * 中等
@@ -49,7 +49,7 @@ public class DiscontinuousSubsequences {
     }
 }
 
-class DiscontinuousSubsequencesSolution2 {
+class SubsequencesSolution2 {
     /**
      * 674. 最长连续递增序列    https://leetcode.cn/problems/longest-continuous-increasing-subsequence/
      * 简单
@@ -120,7 +120,7 @@ class DiscontinuousSubsequencesSolution2 {
  * 1 <= nums1.length, nums2.length <= 1000
  * 0 <= nums1[i], nums2[i] <= 100
  */
-class DiscontinuousSubsequencesSolution3 {
+class SubsequencesSolution3 {
     public int findLength2(int[] nums1, int[] nums2) {
         int[] dp = new int[nums2.length + 1];
         int ans = 0;
@@ -179,7 +179,7 @@ class DiscontinuousSubsequencesSolution3 {
  * 1 <= text1.length, text2.length <= 1000
  * text1 和 text2 仅由小写英文字符组成。
  */
-class DiscontinuousSubsequencesSolution4 {
+class SubsequencesSolution4 {
     public int longestCommonSubsequence(String text1, String text2) {
         int ans = 0;
         int[] dp = new int[text2.length() + 1];
@@ -212,6 +212,57 @@ class DiscontinuousSubsequencesSolution4 {
                 if (t1 == t2) {
                     dp[i][j] = dp[i - 1][j - 1] + 1;
                     ans = Math.max(ans, dp[i][j]);
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        for (int[] d : dp) {
+            System.out.println(Arrays.toString(d));
+        }
+        return ans;
+    }
+}
+
+/**
+ * 1035. 不相交的线
+ * 中等
+ * 在两条独立的水平线上按给定的顺序写下 nums1 和 nums2 中的整数。
+ * 现在，可以绘制一些连接两个数字 nums1[i] 和 nums2[j] 的直线，这些直线需要同时满足满足：
+ * nums1[i] == nums2[j]
+ * 且绘制的直线不与任何其他连线（非水平线）相交。
+ * 请注意，连线即使在端点也不能相交：每个数字只能属于一条连线。
+ * <p>
+ * 以这种方法绘制线条，并返回可以绘制的最大连线数。
+ * <p>
+ * 示例 1：
+ * 输入：nums1 = [1,4,2], nums2 = [1,2,4]
+ * 输出：2
+ * 1    4   2
+ * |      \
+ * 1    2   4
+ * 解释：可以画出两条不交叉的线，如上图所示。
+ * 但无法画出第三条不相交的直线，因为从 nums1[1]=4 到 nums2[2]=4 的直线将与从 nums1[2]=2 到 nums2[1]=2 的直线相交。
+ * 示例 2：
+ * 输入：nums1 = [2,5,1,2,5], nums2 = [10,5,2,1,5,2]
+ * 输出：3
+ * 示例 3：
+ * 输入：nums1 = [1,3,7,1,7,5], nums2 = [1,9,2,5,1]
+ * 输出：2
+ * <p>
+ * 提示：
+ * 1 <= nums1.length, nums2.length <= 500
+ * 1 <= nums1[i], nums2[j] <= 2000
+ */
+class SubsequencesSolution5 {
+    public int maxUncrossedLines(int[] nums1, int[] nums2) {
+        int ans = 0;
+        int[][] dp = new int[nums1.length + 1][nums2.length + 1];
+        for (int i = 1; i <= nums1.length; i++) {
+            for (int j = 1; j <= nums2.length; j++) {
+                if (nums1[i - 1] == nums2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    ans = Math.max(dp[i][j],ans);
                 } else {
                     dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 }
