6bece4b38a202d9853cce3c8e677634511345fc8
JIE <710578873@qq.com>
2023-05-06 14:48:36 +0800

    392. 判断子序列

--- src/com/test/jie/leetCode/dp/Subsequences.java
+++ src/com/test/jie/leetCode/dp/Subsequences.java
@@ -310,3 +310,39 @@ class SubsequencesSolution6 {
         return ans;
     }
 }
+
+/**
+ * 392. 判断子序列
+ * 简单
+ * 给定字符串 s 和 t ，判断 s 是否为 t 的子序列。
+ * 字符串的一个子序列是原始字符串删除一些（也可以不删除）字符而不改变剩余字符相对位置形成的新字符串。（例如，"ace"是"abcde"的一个子序列，而"aec"不是）。
+ * 进阶：
+ * 如果有大量输入的 S，称作 S1, S2, ... , Sk 其中 k >= 10亿，你需要依次检查它们是否为 T 的子序列。在这种情况下，你会怎样改变代码？
+ * <p>
+ * 示例 1：
+ * 输入：s = "abc", t = "ahbgdc"
+ * 输出：true
+ * 示例 2：
+ * 输入：s = "axc", t = "ahbgdc"
+ * 输出：false
+ * <p>
+ * 提示：
+ * 0 <= s.length <= 100
+ * 0 <= t.length <= 10^4
+ * 两个字符串都只由小写字符组成。
+ */
+class SubsequencesSolution7 {
+    public boolean isSubsequence(String s, String t) {
+        if (s.length() > t.length()) {
+            return false;
+        }
+        boolean[] dp = new boolean[s.length()];
+        int idx = 0;
+        for (int i = 0; i < t.length() && idx < s.length(); i++) {
+            if (s.charAt(idx) == t.charAt(i)) {
+                dp[idx++] = true;
+            }
+        }
+        return dp.length == 0 || dp[s.length() - 1];
+    }
+}
