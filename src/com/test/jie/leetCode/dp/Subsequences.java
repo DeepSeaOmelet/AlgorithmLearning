d6c191ff5b708bbe643738b4aa3910d7d4454cdb
JIE <710578873@qq.com>
2023-05-06 18:10:39 +0800

    115. 不同的子序列

--- src/com/test/jie/leetCode/dp/Subsequences.java
+++ src/com/test/jie/leetCode/dp/Subsequences.java
@@ -367,3 +367,68 @@ class SubsequencesSolution7 {
         return dp.length == 0 || dp[s.length() - 1];
     }
 }
+
+/**
+ * 115. 不同的子序列
+ * 困难
+ * 给你两个字符串 s 和 t ，统计并返回在 s 的 子序列 中 t 出现的个数。
+ * <p>
+ * 题目数据保证答案符合 32 位带符号整数范围。
+ * <p>
+ * 示例 1：
+ * 输入：s = "rabbbit", t = "rabbit"
+ * 输出：3
+ * 解释：
+ * 如下所示, 有 3 种可以从 s 中得到 "rabbit" 的方案。
+ * rabbbit
+ * ---- --
+ * rabbbit
+ * ---- --
+ * rabbbit
+ * --- ---
+ * 示例 2：
+ * 输入：s = "babgbag", t = "bag"
+ * 输出：5
+ * 解释：
+ * 如下所示, 有 5 种可以从 s 中得到 "bag" 的方案。
+ * babgbag
+ * -- -
+ * babgbag
+ * --    -
+ * babgbag
+ * -    --
+ * babgbag
+ * -  --
+ * babgbag
+ * ---
+ * <p>
+ * 提示：
+ * 1 <= s.length, t.length <= 1000
+ * s 和 t 由英文字母组成
+ */
+class SubsequencesSolution8 {
+    public int numDistinct(String s, String t) {
+        //动规可以用回溯的思维去考虑
+        //1
+        int[][] dp = new int[s.length() + 1][t.length() + 1];
+        //2
+        for (int i = 0; i <= s.length(); i++) {
+            dp[i][0] = 1;
+        }
+        //3
+        for (int i = 1; i <= s.length(); i++) {
+            char s1 = s.charAt(i - 1);
+            for (int j = 1; j <= t.length(); j++) {
+                char t1 = t.charAt(j - 1);
+                //4
+                if (s1 == t1) {
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                } else {
+                    //dp[i][j]只有一部分组成，不用s[i - 1]来匹配（就是模拟在s中删除这个元素），即：dp[i - 1][j]
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[s.length()][t.length()];
+    }
+}
