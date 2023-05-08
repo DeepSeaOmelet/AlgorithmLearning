510211a5e11dbd1d4ab9e784f32fdfb0469e94db
JIE <710578873@qq.com>
2023-05-08 23:15:59 +0800

    583. 两个字符串的删除操作

--- src/com/test/jie/leetCode/dp/Subsequences.java
+++ src/com/test/jie/leetCode/dp/Subsequences.java
@@ -432,3 +432,54 @@ class SubsequencesSolution8 {
         return dp[s.length()][t.length()];
     }
 }
+
+/**
+ * 583. 两个字符串的删除操作  https://leetcode.cn/problems/delete-operation-for-two-strings/
+ * 中等
+ * 给定两个单词 word1 和 word2 ，返回使得 word1 和  word2 相同所需的最小步数。
+ * 每步 可以删除任意一个字符串中的一个字符。
+ * <p>
+ * 示例 1：
+ * 输入: word1 = "sea", word2 = "eat"
+ * 输出: 2
+ * 解释: 第一步将 "sea" 变为 "ea" ，第二步将 "eat "变为 "ea"
+ * 示例  2:
+ * 输入：word1 = "leetcode", word2 = "etco"
+ * 输出：4
+ * <p>
+ * 提示：
+ * 1 <= word1.length, word2.length <= 500
+ * word1 和 word2 只包含小写英文字母
+ */
+class SubsequencesSolution9 {
+    public int minDistance(String word1, String word2) {
+        //1 dp[i][j]表示需要删除的个数
+        int[][] dp = new int[word1.length() + 1][word2.length() + 1];
+        //3 初始
+        for (int i = 1; i <= word1.length(); i++) {
+            //word1需要删除的个数
+            dp[i][0] = i;
+        }
+        for (int i = 1; i <= word2.length(); i++) {
+            dp[0][i] = i;
+        }
+        for (int i = 1; i <= word1.length(); i++) {
+            char w1 = word1.charAt(i - 1);
+            for (int j = 1; j <= word2.length(); j++) {
+                char w2 = word2.charAt(j - 1);
+                //2
+                if (w1 == w2) {
+                    //表示不用删除元素
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    //表示需要删除元素，删除w1或w2
+                    dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + 1;
+                }
+            }
+        }
+        for (int[] d : dp) {
+            System.out.println(Arrays.toString(d));
+        }
+        return dp[word1.length()][word2.length()];
+    }
+}
