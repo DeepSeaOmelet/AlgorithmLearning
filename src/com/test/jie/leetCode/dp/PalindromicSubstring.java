a31ad391ebf824f7560ea7123a9c6a21b905159a
JIE <710578873@qq.com>
2023-05-11 00:01:26 +0800

    516. 最长回文子序列

--- src/com/test/jie/leetCode/dp/PalindromicSubstring.java
+++ src/com/test/jie/leetCode/dp/PalindromicSubstring.java
@@ -66,7 +66,7 @@ class PalindromicSubstringSolution1 {
  * 中等
  * 给你一个字符串 s ，找出其中最长的回文子序列，并返回该序列的长度。
  * 子序列定义为：不改变剩余字符顺序的情况下，删除某些字符或者不删除任何字符形成的一个序列。
- *
+ * <p>
  * 示例 1：
  * 输入：s = "bbbab"
  * 输出：4
@@ -75,13 +75,64 @@ class PalindromicSubstringSolution1 {
  * 输入：s = "cbbd"
  * 输出：2
  * 解释：一个可能的最长回文子序列为 "bb" 。
- *
+ * <p>
  * 提示：
  * 1 <= s.length <= 1000
  * s 仅由小写英文字母组成
  */
 class PalindromicSubstringSolution2 {
-    public int longestPalindromeSubseq(String s) {
+    //for (int i = s.size() - 1; i >= 0; i--) {  // 注意遍历顺序
+    //            for (int j = i; j < s.size(); j++) {
+    public static int longestPalindromeSubseq2(String s) {
+        char[] cs = s.toCharArray();
+        int len = s.length();
+        //1 优化为滚动数组
+        int[] dp = new int[len];
+        for (int i = 0; i < len; i++) {
+            //初始化，一个字符的回文子序列长度就是1
+            dp[i] = 1;
+            int pre = 0;//用来保存i-1，序列dp滚动数组通用优化技巧
+            for (int j = i - 1; j >= 0; j--) {
+                int temp = dp[j];//用来保存i-1
+                if (cs[i] == cs[j]) {
+                    dp[j] = pre + 2;
+                } else {
+                    dp[j] = Math.max(dp[j], dp[j + 1]);
+                }
+                pre = temp;
+            }
+        }
+        return dp[0];
+    }
 
+    public static int longestPalindromeSubseq(String s) {
+        int len = s.length();
+        //定义dp[i][j]为最长长度
+        int[][] dp = new int[len + 1][len + 1];
+        //3
+        //4
+        for (int i = 0; i < len; i++) {
+            for (int j = i; j >= 0; j--) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    //2
+                    if (i - j <= 1) {
+                        dp[i][j] = i - j + 1;
+                    } else {
+                        //在原来回文基础上+2
+                        dp[i][j] = dp[i - 1][j + 1] + 2;
+                    }
+                } else {
+                    //要么删除i，或者j
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j + 1]);
+                }
+            }
+            //5
+            System.out.println(Arrays.toString(dp[i]));
+        }
+        return dp[len - 1][0];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(longestPalindromeSubseq("bbbab"));
     }
 }
