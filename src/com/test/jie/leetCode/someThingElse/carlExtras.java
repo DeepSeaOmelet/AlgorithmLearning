c0a6380929035ef1319bc949b988cb1bed7c3690
JIE <710578873@qq.com>
2023-05-17 23:33:50 +0800

    update

--- src/com/test/jie/leetCode/someThingElse/carlExtras.java
+++ src/com/test/jie/leetCode/someThingElse/carlExtras.java
@@ -236,6 +236,68 @@ public class carlExtras {
         }
     }
 
+    public void rotate2(int[] nums, int k) {
+        int n = nums.length;
+        int[] clone = nums.clone();
+        for (int i = 0; i < nums.length; i++) {
+            nums[(i + k) % n] = clone[i];
+        }
+    }
+
+    /**
+     * 724. 寻找数组的中心下标
+     * 简单
+     * 给你一个整数数组 nums ，请计算数组的 中心下标 。
+     * 数组 中心下标 是数组的一个下标，其左侧所有元素相加的和等于右侧所有元素相加的和。
+     * 如果中心下标位于数组最左端，那么左侧数之和视为 0 ，因为在下标的左侧不存在元素。这一点对于中心下标位于数组最右端同样适用。
+     * 如果数组有多个中心下标，应该返回 最靠近左边 的那一个。如果数组不存在中心下标，返回 -1 。
+     * <p>
+     * 示例 1：
+     * 输入：nums = [1, 7, 3, 6, 5, 6]
+     * 输出：3
+     * 解释：
+     * 中心下标是 3 。
+     * 左侧数之和 sum = nums[0] + nums[1] + nums[2] = 1 + 7 + 3 = 11 ，
+     * 右侧数之和 sum = nums[4] + nums[5] = 5 + 6 = 11 ，二者相等。
+     * 示例 2：
+     * 输入：nums = [1, 2, 3]
+     * 输出：-1
+     * 解释：
+     * 数组中不存在满足此条件的中心下标。
+     * 示例 3：
+     * 输入：nums = [2, 1, -1]
+     * 输出：0
+     * 解释：
+     * 中心下标是 0 。
+     * 左侧数之和 sum = 0 ，（下标 0 左侧不存在元素），
+     * 右侧数之和 sum = nums[1] + nums[2] = 1 + -1 = 0 。
+     * <p>
+     * 提示：
+     * 1 <= nums.length <= 104
+     * -1000 <= nums[i] <= 1000
+     *
+     * @param nums
+     * @return
+     */
+    public int pivotIndex(int[] nums) {
+        int m = nums.length;
+        //rightSum[i]等于nums[i+1,m)之和
+        int[] rightSum = new int[m + 1];
+        rightSum[m - 1] = nums[m-1];
+        for (int i = m - 2; i >= 0; i--) {
+            rightSum[i] = rightSum[i + 1] + nums[i];
+        }
+        //System.out.println(Arrays.toString(rightSum));
+        int leftSum = 0;
+        for (int i = 0; i < m; i++) {
+            if (leftSum == (rightSum[i]-nums[i])) {
+                return i;
+            }
+            leftSum += nums[i];
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
         Random ran = new Random(System.currentTimeMillis());
         Map<Integer, Integer> map = new HashMap<>();
