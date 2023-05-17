2dfbee051acca0938101a6e57874695bc5caaffb
JIE <710578873@qq.com>
2023-05-17 19:17:09 +0800

    update

--- src/com/test/jie/leetCode/someThingElse/carlExtras.java
+++ src/com/test/jie/leetCode/someThingElse/carlExtras.java
@@ -189,6 +189,53 @@ public class carlExtras {
         }
     }
 
+    /**
+     * 189. 轮转数组
+     * 中等
+     * 给定一个整数数组 nums，将数组中的元素向右轮转 k 个位置，其中 k 是非负数。
+     * <p>
+     * 示例 1:
+     * 输入: nums = [1,2,3,4,5,6,7], k = 3
+     * 输出: [5,6,7,1,2,3,4]
+     * 解释:
+     * 向右轮转 1 步: [7,1,2,3,4,5,6]
+     * 向右轮转 2 步: [6,7,1,2,3,4,5]
+     * 向右轮转 3 步: [5,6,7,1,2,3,4]
+     * 示例 2:
+     * 输入：nums = [-1,-100,3,99], k = 2
+     * 输出：[3,99,-1,-100]
+     * 解释:
+     * 向右轮转 1 步: [99,-1,-100,3]
+     * 向右轮转 2 步: [3,99,-1,-100]
+     * <p>
+     * 提示：
+     * 1 <= nums.length <= 105
+     * -231 <= nums[i] <= 231 - 1
+     * 0 <= k <= 105
+     * <p>
+     * 进阶：
+     * 尽可能想出更多的解决方案，至少有 三种 不同的方法可以解决这个问题。
+     * 你可以使用空间复杂度为 O(1) 的 原地 算法解决这个问题吗？
+     *
+     * @param nums
+     * @param k
+     */
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+    }
+
+    public void reverse(int[] nums, int start, int end) {
+        for (int i = start, j = end; i < j; i++, j--) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+    }
+
     public static void main(String[] args) {
         Random ran = new Random(System.currentTimeMillis());
         Map<Integer, Integer> map = new HashMap<>();
