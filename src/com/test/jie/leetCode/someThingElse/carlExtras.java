7c6e8f5534c054a28407ac9511099f6f0fe6ee39
JIE <710578873@qq.com>
2023-05-19 23:46:09 +0800

    update

--- src/com/test/jie/leetCode/someThingElse/carlExtras.java
+++ src/com/test/jie/leetCode/someThingElse/carlExtras.java
@@ -1,5 +1,7 @@
 package com.test.jie.leetCode.someThingElse;
 
+import com.test.jie.leetCode.tool.ListNode;
+
 import java.util.*;
 
 public class carlExtras {
@@ -494,8 +496,48 @@ public class carlExtras {
                 return mid;
             }
         }
-        System.out.println(left+":"+right);
+        System.out.println(left + ":" + right);
         return left;
+    }
+
+    /**
+     * 24. 两两交换链表中的节点
+     * 中等
+     * 给你一个链表，两两交换其中相邻的节点，并返回交换后链表的头节点。
+     * * 你必须在不修改节点内部的值的情况下完成本题（即，只能进行节点交换）。
+     * <p>
+     * 示例 1：
+     * 输入：head = [1,2,3,4]
+     * 输出：[2,1,4,3]
+     * 示例 2：
+     * 输入：head = []
+     * 输出：[]
+     * 示例 3：
+     * 输入：head = [1]
+     * 输出：[1]
+     * <p>
+     * 提示：
+     * 链表中节点的数目在范围 [0, 100] 内
+     * 0 <= Node.val <= 100
+     *
+     * @param head
+     * @return
+     */
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummyHead = new ListNode(-1);
+        ListNode pre = dummyHead;
+        dummyHead.next = head;
+
+        while (head != null && head.next != null) {
+            ListNode next = head.next;
+            head.next = next.next;
+            next.next = head;
+            pre.next = next;
+            pre = head;
+            head = head.next;
+
+        }
+        return dummyHead.next;
     }
 }
 
