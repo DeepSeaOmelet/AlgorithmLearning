efc0a14b8f4d6e0926348d1f75a6e05a40587c6f
JIE <710578873@qq.com>
2023-05-19 17:27:16 +0800

    update

--- src/com/test/jie/leetCode/someThingElse/carlExtras.java
+++ src/com/test/jie/leetCode/someThingElse/carlExtras.java
@@ -357,7 +357,7 @@ public class carlExtras {
                 l = mid + 1;
                 rightBorder = l;
             }
-            System.out.println(l+":"+r);
+            System.out.println(l + ":" + r);
         }
         return rightBorder;
     }
@@ -377,6 +377,49 @@ public class carlExtras {
             }
         }
         return leftBorder;
+    }
+
+    /**
+     * 922. 按奇偶排序数组 II
+     * 简单
+     * 给定一个非负整数数组 nums，  nums 中一半整数是 奇数 ，一半整数是 偶数 。
+     * 对数组进行排序，以便当 nums[i] 为奇数时，i 也是 奇数 ；当 nums[i] 为偶数时， i 也是 偶数 。
+     * 你可以返回 任何满足上述条件的数组作为答案 。
+     * <p>
+     * 示例 1：
+     * 输入：nums = [4,2,5,7]
+     * 输出：[4,5,2,7]
+     * 解释：[4,7,2,5]，[2,5,4,7]，[2,7,4,5] 也会被接受。
+     * 示例 2：
+     * 输入：nums = [2,3]
+     * 输出：[2,3]
+     * <p>
+     * 提示：
+     * 2 <= nums.length <= 2 * 104
+     * nums.length 是偶数
+     * nums 中一半是偶数
+     * 0 <= nums[i] <= 1000
+     * <p>
+     * 进阶：可以不使用额外空间解决问题吗？
+     *
+     * @param nums
+     * @return
+     */
+    public int[] sortArrayByParityII(int[] nums) {
+        //双指针
+        int odd = 1;//奇数
+        //偶数
+        for (int even = 0; even < nums.length; even += 2) {
+            if (nums[even] % 2 == 1) {
+                while (nums[odd] % 2 != 0) {
+                    odd += 2;
+                }
+                int temp = nums[odd];
+                nums[odd] = nums[even];
+                nums[even] = temp;
+            }
+        }
+        return nums;
     }
 
     public static void main(String[] args) {
@@ -411,3 +454,4 @@ public class carlExtras {
         }
     }
 }
+
